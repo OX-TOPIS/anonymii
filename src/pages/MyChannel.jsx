df398edadb1fbe4f1f943879fc057c71858f5592
baebey <64070234@kmitl.ac.th>
2024-02-04 21:00:11 +0700

    first commit

---
+++ src/pages/MyChannel.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from '../components/Card'
+
+const MyChannel = () => {
+  return (
+    <div className='content'>
+      <div className="headtext">My Channel</div>
+      <Card/>
+    </div>
+  )
+}
+
+export default MyChannel
---
+++ src/pages/chat/Channel.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ChatChannel from "./ChatChannel";
+import Message from "../../components/Message";
+
+const Channel = () => {
+  return (
+    <div className='content flex overscroll-none h-screen'>
+      <div className="w-1/4">
+        <h1 className="headtext">All Messages</h1>
+        <div className="height overflow-y-scroll">
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        </div>
+        
+      </div>
+      <ChatChannel />
+    </div>
+  );
+};
+
+export default Channel;
